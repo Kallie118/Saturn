bf8fbe8df435d4082f01393caa02d6dcca1f3659
Caleb <caleb@Calebs-MacBook-Pro.local>
2019-12-03 13:50:07 -0600

    Added Footer

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ class App extends Component {
     return (
         <div>
           <Navbar />
+          <Footer />
          
         </div>
     );
--- src/pages/footer.js
+++ src/pages/footer.js
@@ -10,25 +10,25 @@ class Footer extends React.Component {
     }
 
     render() {
-        const footerStyle = {
-            clear: 'both',
-            float: 'none',
-            size: '0.3vh'
-        }
 
         return (
             <div>
                 <div className="blank-div">
 
                 </div>
-                <div className="text-center footer fixed-bottom my-auto" style={footerStyle}>
+                <div className="text-center footer fixed-bottom ">
                     <div className="row">
-                        <div className="col my-auto">
-                        <p><small>This is a demonstration of using a backend (express.js) to work with a front end(React.js)</small></p>
+                        <div className="col-5">
+                            <p>Copyright &copy; 2019 Saturn</p> 
+                            <p className="text-primary">Created by team no-babies</p>
                         </div>
-                        <div className="col my-auto">
-                        <p><small>Copyright &copy; 2019 Caleb Hinton</small></p>
+                        
+                        <div className="col-5 text-right footer-icons">
+                            <img src="images/react.png" className="footer-icon" alt="React" />
+                            <img src="images/bootstrap.png" className="footer-icon" alt="Bootstrap" />
+                            <img src="images/firebase.png" className="footer-icon" alt="Firebase" />
                         </div>
+
                     </div>
                 </div>
             </div>
